47a16614133cec501d652dde0858000f924ba18a
David Björklund <david.bjorklund@gmail.com>
2014-06-28 19:24:28 +0200

    cleanup test

--- test.js
+++ test.js
@@ -16,78 +16,73 @@ var remoteDOWN = require('./remotedown')
           }
       )
     }
+  , setup = function (callback) {
+      var serverDb = memDOWN('/does/not/matter')
+
+        , server = remoteDOWN.server(serverDb)
+        , client = remoteDOWN.client()
+
+      server.pipe(client.createRpcStream()).pipe(server)
+
+      callback(client, server, serverDb)
+    }
 
 test('put', function (t) {
-  var serverDb = memDOWN('/does/not/matter')
-
-    , server = remoteDOWN.server(serverDb)
-    , client = remoteDOWN.client()
-
-  server.pipe(client.createRpcStream()).pipe(server)
-
-  client.put(new Buffer('beep'), new Buffer('boop'), function () {
-    serverDb.get(new Buffer('beep'), function (err, value) {
-      t.deepEqual(value, new Buffer('boop'))
-      t.end()
-    })
-  })
-})
-
-test('del', function (t) {
-  var serverDb = memDOWN('/does/not/matter')
-
-    , server = remoteDOWN.server(serverDb)
-    , client = remoteDOWN.client()
-
-  server.pipe(client.createRpcStream()).pipe(server)
-
-  serverDb.put(new Buffer('beep'), new Buffer('boop'), function () {
-    client.del(new Buffer('beep'), function () {
+  setup(function (client, server, serverDb) {
+    client.put(new Buffer('beep'), new Buffer('boop'), function () {
       serverDb.get(new Buffer('beep'), function (err, value) {
-        t.deepEqual(err.message, 'NotFound')
-        t.equal(value, undefined)
+        t.deepEqual(value, new Buffer('boop'))
         t.end()
       })
     })
   })
 })
 
-test('batch', function (t) {
-  var serverDb = memDOWN('/does/not/matter')
-
-    , server = remoteDOWN.server(serverDb)
-    , client = remoteDOWN.client()
-
-  server.pipe(client.createRpcStream()).pipe(server)
-
-  client.batch(
-      [
-          { key: new Buffer('beep'), value: new Buffer('boop'), type: 'put' }
-        , { key: new Buffer('bing'), value: new Buffer('bong'), type: 'put' }
-      ]
-    , function () {
+test('del', function (t) {
+  setup(function (client, server, serverDb) {
+    serverDb.put(new Buffer('beep'), new Buffer('boop'), function () {
+      client.del(new Buffer('beep'), function () {
         serverDb.get(new Buffer('beep'), function (err, value) {
-          t.deepEqual(value, new Buffer('boop'))
-          serverDb.get(new Buffer('bing'), function (err, value) {
-            client.batch(
-                [
-                    { key: new Buffer('beep'), type: 'del' }
-                  , { key: new Buffer('bing'), type: 'del' }
-                ]
-              , function () {
-                  serverDb.get(new Buffer('beep'), function (err, value) {
-                    t.deepEqual(err.message, 'NotFound')
-                    serverDb.get(new Buffer('bing'), function (err, value) {
-                      t.deepEqual(err.message, 'NotFound')
-                      t.end()
-                    })
-                  })
-                }
-            )
-          })
+          t.deepEqual(err.message, 'NotFound')
+          t.equal(value, undefined)
+          t.end()
         })
-      }
-  )
+      })
+    })
+  })
+})
+
+test('batch', function (t) {
+  setup(function (client, server, serverDb) {
+    client.batch(
+        [
+            { key: new Buffer('beep'), value: new Buffer('boop'), type: 'put' }
+          , { key: new Buffer('bing'), value: new Buffer('bong'), type: 'put' }
+        ]
+      , function () {
+          serverDb.get(new Buffer('beep'), function (err, value) {
+            t.deepEqual(value, new Buffer('boop'))
+            serverDb.get(new Buffer('bing'), function (err, value) {
+              client.batch(
+                  [
+                      { key: new Buffer('beep'), type: 'del' }
+                    , { key: new Buffer('bing'), type: 'del' }
+                  ]
+                , function () {
+                    serverDb.get(new Buffer('beep'), function (err, value) {
+                      t.deepEqual(err.message, 'NotFound')
+                      serverDb.get(new Buffer('bing'), function (err, value) {
+                        t.deepEqual(err.message, 'NotFound')
+                        t.end()
+                      })
+                    })
+                  }
+              )
+            })
+          })
+        }
+    )
+  })
 })
 
 test('multiple batches directly after each other', function (t) {
@@ -97,7 +92,6 @@ test('multiple batches directly after each other', function (t) {
 
     , server = remoteDOWN.server(serverDb)
     , client = remoteDOWN.client()
-    , array = []
     , bufferingStream = createBufferingStream()
 
   server
