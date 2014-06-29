6e8ab83643c2092859f6aa87316c48767063f55c
David Björklund <david.bjorklund@gmail.com>
2014-06-29 17:14:38 +0200

    all relevant tests form abstract-leveldown is now passing!

--- test/abstract-leveldown-test.js
+++ test/abstract-leveldown-test.js
@@ -5,10 +5,10 @@ var test       = require('tape')
 
 /*** compatibility with basic LevelDOWN API ***/
 
-// meh require('abstract-leveldown/abstract/leveldown-test').args(remoteDOWN, test, testCommon)
+// require('abstract-leveldown/abstract/leveldown-test').args(remoteDOWN, test, testCommon)
 
-// require('abstract-leveldown/abstract/open-test').args(remoteDOWN, test, testCommon)
-// require('abstract-leveldown/abstract/open-test').open(remoteDOWN, test, testCommon)
+require('abstract-leveldown/abstract/open-test').args(remoteDOWN, test, testCommon)
+require('abstract-leveldown/abstract/open-test').open(remoteDOWN, test, testCommon)
 
 require('abstract-leveldown/abstract/del-test').all(remoteDOWN, test, testCommon)
 
@@ -19,10 +19,10 @@ require('abstract-leveldown/abstract/put-test').all(remoteDOWN, test, testCommon
 require('abstract-leveldown/abstract/put-get-del-test').all(remoteDOWN, test, testCommon, testBuffer)
 
 require('abstract-leveldown/abstract/batch-test').all(remoteDOWN, test, testCommon)
-// require('abstract-leveldown/abstract/chained-batch-test').all(remoteDOWN, test, testCommon)
+require('abstract-leveldown/abstract/chained-batch-test').all(remoteDOWN, test, testCommon)
 
-// require('abstract-leveldown/abstract/close-test').close(remoteDOWN, test, testCommon)
+require('abstract-leveldown/abstract/close-test').close(remoteDOWN, test, testCommon)
 
 require('abstract-leveldown/abstract/iterator-test').all(remoteDOWN, test, testCommon)
 
-// require('abstract-leveldown/abstract/ranges-test').all(remoteDOWN, test, testCommon)
+require('abstract-leveldown/abstract/ranges-test').all(remoteDOWN, test, testCommon)
